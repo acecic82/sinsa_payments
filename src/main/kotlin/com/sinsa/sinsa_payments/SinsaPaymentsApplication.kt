332009bcf3a9732685118e85857e7ca9d65acc90
acecic82 <102938al@naver.com>
2024-10-29 03:50:02 +0900

    first commit

---
+++ src/main/kotlin/com/sinsa/sinsa_payments/SinsaPaymentsApplication.kt
@@ -0,0 +1,11 @@
+package com.sinsa.sinsa_payments
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SinsaPaymentsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SinsaPaymentsApplication>(*args)
+}
---
+++ src/test/kotlin/com/sinsa/sinsa_payments/SinsaPaymentsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sinsa.sinsa_payments
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SinsaPaymentsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
