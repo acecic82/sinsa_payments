04ab00b82c1792dcb89aecf68971940d90a69c29
acecic82 <102938al@naver.com>
2024-10-31 02:19:26 +0900

    수정(FreePointRepository, FreePointRepositoryImpl 파일)
    수정 내용
    - memberId 와 ExpiredDate 를 기준으로 유효한 포인트를 뽑고
    - expiredDate 를 기준으로 정렬한 값을 뽑아주는 method 추가
    - 빈등록을 위한 annotation 추가

--- src/main/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointRepository.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointRepository.kt
@@ -2,10 +2,12 @@ package com.sinsa.sinsa_payments.persistence.repository
 
 import com.sinsa.sinsa_payments.persistence.entity.FreePointEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDateTime
 
 interface FreePointRepository : JpaRepository<FreePointEntity, Long>, FreePointRepositoryCustom {
 }
 
 interface  FreePointRepositoryCustom {
-
+    //ExpiredDate 를 기준으로 오래된 순서로 가져오는 메서드
+    fun findPointByMemberId(memberId: Long, expiredDateTime: LocalDateTime): List<FreePointEntity>
 }
--- src/main/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointRepositoryImpl.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointRepositoryImpl.kt
@@ -2,6 +2,16 @@ package com.sinsa.sinsa_payments.persistence.repository
 
 import com.sinsa.sinsa_payments.persistence.entity.FreePointEntity
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
+import com.sinsa.sinsa_payments.persistence.entity.QFreePointEntity.Companion.freePointEntity
+import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 
+@Repository
 class FreePointRepositoryImpl : QuerydslRepositorySupport(FreePointEntity::class.java), FreePointRepositoryCustom {
+    override fun findPointByMemberId(memberId: Long, expiredDateTime: LocalDateTime): List<FreePointEntity> {
+        return from(freePointEntity)
+            .where(freePointEntity.memberId.eq(memberId).and(freePointEntity.expiredDate.loe(expiredDateTime)))
+            .orderBy(freePointEntity.expiredDate.asc())
+            .fetch()
+    }
 }
