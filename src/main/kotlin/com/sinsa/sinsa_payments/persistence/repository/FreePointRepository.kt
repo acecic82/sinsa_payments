4113fe20e7e072faa731aa94351a45ae5b0873a3
acecic82 <102938al@naver.com>
2024-10-31 02:26:04 +0900

    수정 (FreePointRepository 파일)
    수정 내용
    - 사용할 포인트 가져오는 쿼리 비관적 락 추가

--- src/main/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointRepository.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointRepository.kt
@@ -1,7 +1,9 @@
 package com.sinsa.sinsa_payments.persistence.repository
 
 import com.sinsa.sinsa_payments.persistence.entity.FreePointEntity
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import java.time.LocalDateTime
 
 interface FreePointRepository : JpaRepository<FreePointEntity, Long>, FreePointRepositoryCustom {
@@ -9,5 +11,6 @@ interface FreePointRepository : JpaRepository<FreePointEntity, Long>, FreePointR
 
 interface  FreePointRepositoryCustom {
     //ExpiredDate 를 기준으로 오래된 순서로 가져오는 메서드
+    @Lock(LockModeType.PESSIMISTIC_READ)
     fun findPointByMemberId(memberId: Long, expiredDateTime: LocalDateTime): List<FreePointEntity>
 }
