e589a34a85248eb575053e3a4cd30d64732910d6
acecic82 <102938al@naver.com>
2024-10-31 04:12:26 +0900

    수정 (FreePointRepositoryTest 파일)
    수정 내용
    - 전체, 1일전, 없는 경우 테스트 코드 추가

--- src/test/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointRepositoryTest.kt
+++ src/test/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointRepositoryTest.kt
@@ -6,6 +6,7 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.extensions.spring.SpringTestExtension
 import io.kotest.extensions.spring.SpringTestLifecycleMode
 import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.context.annotation.Import
@@ -54,8 +55,8 @@ class FreePointRepositoryTest(
             context("memberId를 찾고 expiredDate 기준으로 정렬된 결과 테스트") {
                 val memberId = 1L
 
-                it("현재 시간 기준으로 검색시 전체가 정렬된 결과가 나와야 한다.") {
-                    val now = LocalDateTime.now()
+                it("과거 시간 기준으로 검색시 전체가 정렬된 결과가 나와야 한다.") {
+                    val now = LocalDateTime.now().minusDays(3L)
 
                     val realResult = freePointRepository.findPointByMemberId(memberId, now).map {
                         FreePointEntity(
@@ -80,6 +81,51 @@ class FreePointRepositoryTest(
                             throw AssertionError("결과가 올바르게 정렬되어 있지 않습니다.")
                         }
                     }
+                }
+
+                it("2일 기준으로 검색시 1일전 이상만 나와야 한다.") {
+                    // 저장 시점보다 현재 시점이 더 미래이기 때문에 1일전 결과만 나와야 한다.
+                    val now = LocalDateTime.now().minusDays(2L)
+
+                    val realResult = freePointRepository.findPointByMemberId(memberId, now).map {
+                        FreePointEntity(
+                            it.id,
+                            it.memberId,
+                            BigDecimal.valueOf(it.point.toLong()),
+                            it.manual,
+                            it.expiredDate
+                        )
+                    }
+                    val expectedResult = freePointListBeforeOneDay.filter { it.memberId == memberId }
+                    var orderCheckItem = LocalDateTime.now().minusDays(3L)
+
+                    realResult.forEachIndexed{ index, it ->
+                        it shouldBeIn(expectedResult)
+
+                        if (it.expiredDate.isAfter(orderCheckItem)) {
+                            orderCheckItem = it.expiredDate
+                        }
+                        else {
+                            throw AssertionError("결과가 올바르게 정렬되어 있지 않습니다.")
+                        }
+                    }
+                }
+
+                it("결과가 없는 경우 빈 배열이 나와야한다.") {
+                    val now = LocalDateTime.now()
+
+                    val realResult = freePointRepository.findPointByMemberId(memberId, now).map {
+                        FreePointEntity(
+                            it.id,
+                            it.memberId,
+                            BigDecimal.valueOf(it.point.toLong()),
+                            it.manual,
+                            it.expiredDate
+                        )
+                    }
+
+                    realResult.size shouldBe 0
+
                 }
             }
         }
