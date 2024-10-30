5dcb81e5cfcc57932864c1939fce99eaa6b7433f
acecic82 <102938al@naver.com>
2024-10-31 04:00:45 +0900

    수정 (FreePointSnapshotRepository 파일)
    수정 내용
    - 오타 수정 FreePointRepositoryCustom -> FreePointSnapshotRepositoryCustom

--- src/main/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointSnapshotRepository.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointSnapshotRepository.kt
@@ -3,7 +3,7 @@ package com.sinsa.sinsa_payments.persistence.repository
 import com.sinsa.sinsa_payments.persistence.entity.FreePointSnapshotEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface FreePointSnapshotRepository : JpaRepository<FreePointSnapshotEntity, Long>, FreePointRepositoryCustom {
+interface FreePointSnapshotRepository : JpaRepository<FreePointSnapshotEntity, Long>, FreePointSnapshotRepositoryCustom {
 }
 
 interface  FreePointSnapshotRepositoryCustom {
