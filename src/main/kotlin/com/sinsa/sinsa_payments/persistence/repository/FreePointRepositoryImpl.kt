17bd00802c2bacdbde63df09d8d32d8c6974f579
acecic82 <102938al@naver.com>
2024-10-31 04:01:43 +0900

    수정 (FreePointRepositoryImpl 파일)
    수정 내용
    - 비관적락 사용을 위한 factory 추가와 비관적락 사용 부분 수정

--- src/main/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointRepositoryImpl.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointRepositoryImpl.kt
@@ -1,17 +1,23 @@
 package com.sinsa.sinsa_payments.persistence.repository
 
+import com.querydsl.jpa.impl.JPAQueryFactory
 import com.sinsa.sinsa_payments.persistence.entity.FreePointEntity
-import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import com.sinsa.sinsa_payments.persistence.entity.QFreePointEntity.Companion.freePointEntity
+import jakarta.persistence.LockModeType
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import org.springframework.stereotype.Repository
 import java.time.LocalDateTime
 
 @Repository
-class FreePointRepositoryImpl : QuerydslRepositorySupport(FreePointEntity::class.java), FreePointRepositoryCustom {
+class FreePointRepositoryImpl(
+    private val queryFactory : JPAQueryFactory
+) : QuerydslRepositorySupport(FreePointEntity::class.java), FreePointRepositoryCustom {
     override fun findPointByMemberId(memberId: Long, expiredDateTime: LocalDateTime): List<FreePointEntity> {
-        return from(freePointEntity)
+        return queryFactory.select(freePointEntity)
+            .from(freePointEntity)
             .where(freePointEntity.memberId.eq(memberId).and(freePointEntity.expiredDate.loe(expiredDateTime)))
             .orderBy(freePointEntity.expiredDate.asc())
+            .setLockMode(LockModeType.PESSIMISTIC_READ)
             .fetch()
     }
 }
