4967695b65b5d36d5cd514186dbaa2a8cc77f027
acecic82 <102938al@naver.com>
2024-11-01 04:20:12 +0900

    수정 (SavePointPolicyUseCase 인터페이스 파일)
    수정 내용
    - setExpiredDate 메서드 추가

--- src/main/kotlin/com/sinsa/sinsa_payments/api/in/port/SavePointPolicyUseCase.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/api/in/port/SavePointPolicyUseCase.kt
@@ -4,5 +4,8 @@ import com.sinsa.sinsa_payments.api.`in`.application.vo.PointPolicyVO
 
 interface SavePointPolicyUseCase {
     fun saveMaxAccumulatedPoint(point: Long) : PointPolicyVO
+
     fun saveMaxHeldPoint(point: Long) : PointPolicyVO
+
+    fun saveExpiredDateOfDay(days: Long) : PointPolicyVO
 }
