16a9a02c5b00039d310e88c9201e1fc8635e1b38
acecic82 <102938al@naver.com>
2024-11-01 04:04:01 +0900

    수정 (PointPolicy 도메인 파일)
    수정 내용
    - expiredDate 관련 정책 필드 추가
    - 추가 필드 validation and set 할 수 있는 메서드 추가
    - const value 추가

--- src/main/kotlin/com/sinsa/sinsa_payments/domain/PointPolicy.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/domain/PointPolicy.kt
@@ -5,7 +5,8 @@ import java.math.BigDecimal
 data class PointPolicy(
     val id: Long? = null,
     var maxAccumulatedPoint: BigDecimal,
-    var maxHeldPoint: BigDecimal
+    var maxHeldPoint: BigDecimal,
+    var dayOfExpiredDate: Long
 ) {
     fun checkValidationMaxAccumulatedPoint(point: Long) : Boolean {
         return point in 1..DEFAULT_MAX_ACCUMULATED_POINT
@@ -13,6 +14,10 @@ data class PointPolicy(
 
     fun checkValidationMaxHeldPoint(point: Long) : Boolean {
         return point in 1..DEFAULT_MAX_HELD_POINT
+    }
+
+    fun checkValidationDayOfExpiredDate(days: Long): Boolean {
+        return days in 1L..<DEFAULT_MAX_DAY_OF_EXPIRED_DATE
     }
 
     fun changMaxAccumulatedPoint(point: BigDecimal) {
@@ -23,11 +28,18 @@ data class PointPolicy(
         this.maxAccumulatedPoint = point
     }
 
+    fun changDayOfExpiredDate(days: Long) {
+        this.dayOfExpiredDate = days
+    }
+
     companion object {
         const val DEFAULT_MAX_ACCUMULATED_POINT = 100_000L
         const val DEFAULT_MAX_HELD_POINT = 1_000_000L
+        const val DEFAULT_MAX_DAY_OF_EXPIRED_DATE = 365L * 5
+        const val DEFAULT_DAY_OF_EXPIRED_DATE = 365L
 
         const val REDIS_MAX_ACCUMULATED_POINT_KEY_NAME = "PointPolicyMaxAccumulatedPointKey"
         const val REDIS_MAX_HELD_POINT_KEY_NAME = "PointPolicyMaxHeldPointKey"
+        const val REDIS_DAY_OR_EXPIRED_DATE_KEY_NAME = "PointPolicyDayOfExpiredDateKey"
     }
 }
