1a5ab958db1a17f3e7e3b76847c1ea8cc9d66b75
acecic82 <102938al@naver.com>
2024-11-01 04:05:02 +0900

    수정 (ExceptionCode 파일)
    수정 내용
    - expiredDate 관련 메세지 추가

--- src/main/kotlin/com/sinsa/sinsa_payments/common/exception/enum/ExceptionCode.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/common/exception/enum/ExceptionCode.kt
@@ -9,6 +9,7 @@ enum class ExceptionCode(
     //Point_Policy
     POINT_POLICY_INVALID_ACCUMULATED("유효하지 않은 적립금액이 저장 요청되었습니다 point : "),
     POINT_POLICY_INVALID_HELD("유효하지 않은 보유가능한 금액이 저장 요청되었습니다 point : "),
+    POINT_POLICY_INVALID_DAY_OF_EXPIRED_DATE("유효하지 않은 만료일 저장 요청되었습니다 days : "),
 
     //Redis
     POINT_POLICY_REDIS_SETTING_FAIL("레디스 setting 에 실패하였습니다."),
