6536f0b2ae0a006c5a3be473658e10ac14a149d1
acecic82 <102938al@naver.com>
2024-10-31 17:42:55 +0900

    수정 (RedisService 파일)
    수정 내용
    - Redis key 기본 셋팅 시간 1시간으로 수정

--- src/main/kotlin/com/sinsa/sinsa_payments/common/redis/service/RedisService.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/common/redis/service/RedisService.kt
@@ -6,7 +6,7 @@ import java.time.Duration
 interface RedisService<T> {
     fun get(key: String) : T
 
-    fun set(key: String, value: T, duration: Duration = Duration.ofMinutes(10L))
+    fun set(key: String, value: T, duration: Duration = Duration.ofMinutes(60L))
 
     fun getRLock(key: String): RLock
 }
