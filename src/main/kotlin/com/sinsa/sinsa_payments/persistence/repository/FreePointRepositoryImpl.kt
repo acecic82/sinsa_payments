7b2ec1c809142bbdb6441a7b2ce214a337d36078
acecic82 <102938al@naver.com>
2024-11-01 10:02:16 +0900

    수정 (FreePointRepositoryImpl 파일)
    수정 내용
    - 포인트가 없는 경우를 제외하고 조회하도록 변경

--- src/main/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointRepositoryImpl.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/persistence/repository/FreePointRepositoryImpl.kt
@@ -6,6 +6,7 @@ import com.sinsa.sinsa_payments.persistence.entity.QFreePointEntity.Companion.fr
 import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import org.springframework.stereotype.Repository
+import java.math.BigDecimal
 import java.time.LocalDateTime
 
 @Repository
@@ -15,7 +16,8 @@ class FreePointRepositoryImpl(
     override fun findPointByMemberId(memberId: Long, expiredDateTime: LocalDateTime): List<FreePointEntity> {
         return queryFactory.select(freePointEntity)
             .from(freePointEntity)
-            .where(freePointEntity.memberId.eq(memberId).and(freePointEntity.expiredDate.gt(expiredDateTime)))
+            .where(freePointEntity.memberId.eq(memberId).and(freePointEntity.expiredDate.gt(expiredDateTime).and(
+                freePointEntity.point.gt(BigDecimal.ZERO))))
             .orderBy(freePointEntity.expiredDate.asc())
             .setLockMode(LockModeType.PESSIMISTIC_READ)
             .fetch()
