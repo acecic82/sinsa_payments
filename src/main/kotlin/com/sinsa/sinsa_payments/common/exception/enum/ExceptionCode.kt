0b9fe301bcf2c5097ce5f4d76eb9b001a6a8dfa6
acecic82 <102938al@naver.com>
2024-11-03 21:30:44 +0900

    수정 (ExceptionCode 관련 파일)
    수정 내용
    - 만료된 포인트는 취소할 수 없는 메세지 추가

--- src/main/kotlin/com/sinsa/sinsa_payments/common/exception/enum/ExceptionCode.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/common/exception/enum/ExceptionCode.kt
@@ -3,9 +3,6 @@ package com.sinsa.sinsa_payments.common.exception.enum
 enum class ExceptionCode(
     val message: String
 ) {
-    //SUCCESS
-    SUCCESS("성공"),
-
     //Point_Policy
     POINT_POLICY_INVALID_ACCUMULATED("유효하지 않은 적립금액이 저장 요청되었습니다 Point : "),
     POINT_POLICY_INVALID_HELD("유효하지 않은 보유가능한 금액이 저장 요청되었습니다 Point : "),
@@ -15,6 +12,7 @@ enum class ExceptionCode(
     FREE_POINT_ACCUMULATED_TOO_MUCH("적립 요청한 포인트가 1회 적립 가능한 포인트보다 많습니다. Point : "),
     FREE_POINT_HELD_TOO_MUCH("적립된 포인트가 많아 적립할 수 없습니다."),
     FREE_POINT_ALREADY_USED("적립된 포인트 일부가 사용되어 취소할 수 없습니다."),
+    FREE_POINT_ALREADY_EXPIRE("적립된 포인트가 만료되어 취소할 수 없습니다."),
     FREE_POINT_NOT_FOUND("적립된 포인트 정보를 찾을 수 없습니다 PointId : "),
     FREE_POINT_APPROVAL_TOO_MUCH("사용하려는 포인트가 보유한 포인트보다 많습니다."),
     FREE_POINT_CANCEL_TOO_MUCH("취소하려는 포인트가 사용한 포인트보다 많습니다."),
