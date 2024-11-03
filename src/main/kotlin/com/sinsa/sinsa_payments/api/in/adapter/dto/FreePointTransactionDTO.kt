d75a43a15a837942d5d8df594a12ae990a01ec8d
acecic82 <102938al@naver.com>
2024-11-03 16:24:35 +0900

    수정 (FreePointTransaction 파일)
    수정 내용
    - 포인트 사용, 취소가 같은 DTO 로 사용하기 위해 이름을 transaction 으로 변경

--- src/main/kotlin/com/sinsa/sinsa_payments/api/in/adapter/dto/FreePointTransactionDTO.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/api/in/adapter/dto/FreePointTransactionDTO.kt
@@ -1,14 +1,14 @@
 package com.sinsa.sinsa_payments.api.`in`.adapter.dto
 
-import com.sinsa.sinsa_payments.api.`in`.application.vo.FreePointUseVO
+import com.sinsa.sinsa_payments.api.`in`.application.vo.FreePointTransactionVO
 import java.math.BigDecimal
 
-data class FreePointUseDTO (
+data class FreePointTransactionDTO (
     val memberId: Long,
     val point: String,
     val orderId: String
 ) {
-    fun toVO() = FreePointUseVO (
+    fun toVO() = FreePointTransactionVO (
         memberId = this.memberId,
         point = BigDecimal(this.point),
         orderId = this.orderId,
--- src/main/kotlin/com/sinsa/sinsa_payments/api/in/application/vo/FreePointTransactionVO.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/api/in/application/vo/FreePointTransactionVO.kt
@@ -2,7 +2,7 @@ package com.sinsa.sinsa_payments.api.`in`.application.vo
 
 import java.math.BigDecimal
 
-data class FreePointUseVO(
+data class FreePointTransactionVO(
     val memberId: Long,
     val point: BigDecimal,
     val orderId: String
