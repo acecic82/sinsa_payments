e73d44e59b593dcd3722d8c8dce7d9886e0c1c4f
acecic82 <102938al@naver.com>
2024-11-09 13:29:10 +0900

    수정 (JpaConfig 관련 파일)
    수정 내용
    
    - 겹치는 annotation 삭제

--- src/main/kotlin/com/sinsa/sinsa_payments/persistence/configuration/JpaConfig.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/persistence/configuration/JpaConfig.kt
@@ -5,11 +5,9 @@ import jakarta.persistence.EntityManager
 import jakarta.persistence.PersistenceContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 
 @Configuration
-@EnableJpaAuditing
 class JPAConfig {
     @PersistenceContext
     private val entityManager: EntityManager? = null
