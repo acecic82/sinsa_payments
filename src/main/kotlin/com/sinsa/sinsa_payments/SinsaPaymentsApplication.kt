c9834c349a70a6d6b44cc9c250bb894356be8e5b
acecic82 <102938al@naver.com>
2024-11-09 13:30:33 +0900

    수정 (Application 관련 파일)
    수정 내용
    
    - 기본 DataSource 예외 추가
    - ComponentScan 하기 위한 패키지 지정

--- src/main/kotlin/com/sinsa/sinsa_payments/SinsaPaymentsApplication.kt
+++ src/main/kotlin/com/sinsa/sinsa_payments/SinsaPaymentsApplication.kt
@@ -1,10 +1,18 @@
 package com.sinsa.sinsa_payments
 
+import com.sinsa.sinsa_payments.persistence.configuration.EnableDataSourceConfigration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@SpringBootApplication
+@SpringBootApplication(
+	exclude = [
+		DataSourceAutoConfiguration::class
+	],
+	scanBasePackages = ["com.sinsa.sinsa_payments"]
+)
+@EnableDataSourceConfigration
 @EnableScheduling
 class SinsaPaymentsApplication
 
